6a6f57cd7202c3abcaef9aaa64163d749905395f
Ghazal Maallawi <ghazalmaalewi@gmail.com>
2026-08-30 18:19:17 +0300

    fix bug

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,26 +1,21 @@
-import { NextResponse } from "next/server";
-
-let tasks = [
-  {
-    id: 1,
-    title: "Review Route Handlers",
-    completed: false
-  }
+let posts = [
+    {
+        id: 1,
+        title: "Review Route Handlers",
+        completed: false
+    }
 ];
 
-export async function GET() {
-  return NextResponse.json({data: tasks}, { status: 200 }); 
-}
-
-export async function POST(request: Request) {
-  const body = await request.json();
-  if (!body.title || body.title.trim() === "") 
-    { return NextResponse.json({ message: "Title is required" }, { status: 400 }); }
-  const newTask = {
-    id: tasks.length + 1,
-    title: body.title,
-    completed: false
-  };
-  tasks.push(newTask);
-  return NextResponse.json(newTask, { status: 201 });
+export async function PATCH(
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const { id } = await params;
+    const body = await request.json();
+    const post = posts.find((p) => p.id === Number(id));
+    if (!post) {
+        return  Response.json({ message: "Route not found" }, { status: 404 });
+    }
+    post.title = body.title;
+    return Response.json(post);
 }
